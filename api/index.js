e9b07846d7dee5beec6c1df1fb4aab6e8f86ad5a
sai <asaikrishnachary103@gmail.com>
2025-06-14 13:42:04 +0530

    sai

--- api/index.js
+++ api/index.js
@@ -1,16 +1,9 @@
 const express = require('express');
+const userRoutes = require('../routes/userRoutes');
+
 const app = express();
-const port = 5000;
-
-// Middleware
 app.use(express.json());
-
-// Routes
-const userRoutes = require('./../routes/userRoutes');
-
-
 app.use('/', userRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// 👇 This is the key line for Vercel
+module.exports = app;
