493a8ce6a930aaf34e3a2aa67ed913412c350010
sai <asaikrishnachary103@gmail.com>
2025-06-30 10:44:47 +0530

    sai

--- db.js
+++ db.js
@@ -1,24 +1,3 @@
-// const mysql = require("mysql2/promise");
-
-// const connection = mysql.createPool({
-//   host: 'localhost',       // or your DB host
-//   user: 'root',            // your DB username
-//   password: '', // your DB password
-//   database: 'helperplace', 
-  
-// });
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('❌ Error connecting to MySQL:', err.stack);
-//     return;
-//   }
-//   console.log('✅ Connected to MySQL as ID ' + connection.threadId);
-// });
-
-// module.exports = connection;
-
-
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
