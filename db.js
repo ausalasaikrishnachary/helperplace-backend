2255ccec6620fd6ff1e5dabc5eaf4b48eeacf156
Tharun <tharunkumarreddy1212@gmail.com>
2025-09-22 13:15:23 +0530

    Employee Navbar and Employer Navbar

--- db.js
+++ db.js
@@ -8,4 +8,12 @@ const db = mysql.createPool({
   port: 3306
 });
 
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "Gudnet",
+//   port: 4306
+// });
+
 module.exports = db;
