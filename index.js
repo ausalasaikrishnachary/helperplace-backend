e9b34ed91b0dc24893f4620af2c9b4ffb2dbf3ec
Saikrushna <kadarlasaikrushna01@gmail.com>
2025-10-03 13:52:41 +0530

    Saikrushna

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ require('./routes/inactivityChecker');
 
 // Schedule daily subscription reminder at 1:30 PM IST (08:00 AM UTC)
 // ✅ Schedule daily subscription reminder at 1:30 PM IST
-cron.schedule('22 11 * * *', async () => {
+cron.schedule('53 12 * * *', async () => {
   console.log('⏰ [IST] Running scheduled subscription reminders at 1:30 PM');
   try {
     const result = await emailService.checkAndSendSubscriptionReminders(db);
--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -2,8 +2,8 @@
 const nodemailer = require('nodemailer');
 
 // Admin email credentials
-const ADMIN_EMAIL = 'iiiqbets01@gmail.com';
-const ADMIN_APP_PASSWORD = 'rava xoel gzai rkgx';
+const ADMIN_EMAIL = 'gulfworker01@gmail.com';
+const ADMIN_APP_PASSWORD = 'ubcb okcm ckne xapq';
 
 // Create transporter with better configuration
 const transporter = nodemailer.createTransport({
