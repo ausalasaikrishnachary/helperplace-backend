0dac3e190dc130051025464952ebae64830b78dc
Ganesh Mohane <ganeshmohane5@gmail.com>
2026-02-04 22:16:21 +0530

    Update database credentials for production

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
-
+// Prod Credentials
 const db = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "",
+  user: "gudnet_app",
+  password: "StrongPass@123",
   database: "Gudnet",
   port: 3306
   
