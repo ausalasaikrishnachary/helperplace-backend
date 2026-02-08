bc30ad3b1324b2443ff7a89b209b30d1300f8360
Saikrushna <kadarlasaikrushna01@gmail.com>
2026-02-08 15:53:08 +0530

    Sai

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 const mysql = require("mysql2/promise");
 
 // Prod Credentials
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "Gudnet",
+//   port: 3306
+  
+// }); 
+
 const db = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "",
+  user: "gudnet_app",
+  password: "StrongPass@123",
   database: "Gudnet",
   port: 3306
   
